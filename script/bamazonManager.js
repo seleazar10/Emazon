4df0b00fdc4bac587e1ebe9a54d53a9d8f1049bb
Stama Eleazar <eleoatam10@gmail.com>
2019-09-09 21:58:52 -0500

    inquirer connected on bamazon

--- script/bamazonManager.js
+++ script/bamazonManager.js
@@ -1,17 +1,41 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-var begin = function(){
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'selectPrompt',
-            message: 'Select from the menu below:',
-            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
-        }
-    ]).then(function(ans){
-        console.log(ans)
-    })
-}
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: '232400',
+	database: 'bamazon_db'
+});
 
-begin()
+connection.connect(function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Connected as id: ' + connection.threadId);
+		begin();
+	}
+});
+
+
+var begin = function() {
+	inquirer
+		.prompt([
+			{
+				type: 'list',
+				name: 'selectPrompt',
+				message: 'Select from the menu below:',
+				choices: [ 'View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product' ]
+			}
+		])
+		.then(function(ans) {
+			console.log(ans);
+		});
+};
+
+// begin();
+
+// var sale = function() {
+// 	console.log('id');
+// };
